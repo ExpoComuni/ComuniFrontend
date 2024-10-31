1534c3e5cc2a4f212a9036c0da7871830979a002
Comuni <expocommuni@gmail.com>
2024-10-31 14:45:09 -0600

    min update

--- src/api/base.js
+++ src/api/base.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
+    baseURL: import.meta.env.VITE_BASE_URL || "localhost:3000",
     timeout: 10000,
 })
 
